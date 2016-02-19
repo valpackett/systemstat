db07a6f710e42fd4a94458bdb2b8f7941eef9c31
Greg V <greg@unrelenting.technology>
2016-02-19 21:42:02 +0300

    add mounts

--- src/data.rs
+++ src/data.rs
@@ -1,5 +1,18 @@
+#[derive(Debug, Clone)]
 pub struct LoadAverage {
     pub one: f32,
     pub five: f32,
     pub fifteen: f32,
+}
+
+#[derive(Debug, Clone)]
+pub struct Filesystem {
+    pub files: u64,
+    pub free_bytes: u64,
+    pub avail_bytes: u64,
+    pub total_bytes: u64,
+    pub name_max: u64,
+    pub fs_type: String,
+    pub fs_mounted_from: String,
+    pub fs_mounted_on: String
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,5 @@ extern crate libc;
 pub mod platform;
 pub mod data;
 
-pub use self::platform::*;
+pub use self::platform::PlatformImpl as System;
 pub use self::data::*;
-
-pub static SYSTEM: PlatformImpl = PlatformImpl;
