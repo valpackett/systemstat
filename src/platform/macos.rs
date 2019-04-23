67052a114c317cdcc7262c24e58faa1a46fb2178
Fenhl <fenhl@fenhl.net>
2019-04-23 21:52:02 +0000

    Implement mount_at for macOS

--- src/platform/macos.rs
+++ src/platform/macos.rs
@@ -83,8 +83,14 @@ impl Platform for PlatformImpl {
         Ok(mounts.iter().map(statfs_to_fs).collect::<Vec<_>>())
     }
 
-    fn mount_at<P: AsRef<path::Path>>(&self, _: P) -> io::Result<Filesystem> {
-        Err(io::Error::new(io::ErrorKind::Other, "Not supported"))
+    fn mount_at<P: AsRef<path::Path>>(&self, path: P) -> io::Result<Filesystem> {
+        self.mounts()
+            .and_then(|mounts| {
+                mounts
+                    .into_iter()
+                    .find(|mount| path::Path::new(&mount.fs_mounted_on) == path.as_ref())
+                    .ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "No such mount"))
+        })
     }
 
     fn block_device_statistics(&self) -> io::Result<BTreeMap<String, BlockDeviceStats>> {
